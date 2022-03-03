214d771e64757bd1955ec076c8d7fc31aefa13ab
diobrando4 <joestar373@gmail.com>
2022-03-03 21:57:03 +0100

    bullet update
    still need to change the physics at some point

--- P10UnityURP/Assets/Scripts/BulletController.cs
+++ P10UnityURP/Assets/Scripts/BulletController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    // i was told that in order to avoid having to put rigidbodies on every single wall; 
+    // i instead have to use rigidbody and not transform (translate) to move the bullet 
+    // and to use continuos collision detection (CCD) (instead of collision enter?) 
+    // and i think you need to use fixedupdate; since that is using the physics engine?
+
     public float speed;
 
     public float lifeTime = 3.0f; // in seconds?
@@ -23,7 +28,7 @@ public class BulletController : MonoBehaviour
         }
     }
 
-    // for when bullets hit enemies
+    // for when bullets hit something (seems like you need rigidbody to make this work)
     void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "Enemy")
@@ -31,6 +36,16 @@ public class BulletController : MonoBehaviour
             other.gameObject.GetComponent<ZombieHealthManager>().HurtZombie(damageGiven);
 
             // destroys the bullet when hitting the enemy
+            Destroy(gameObject);
+        }
+        if(other.gameObject.tag == "Wall")
+        {
+            //Debug.Log("a bullet hit a wall");
+            // destroys the bullet when hitting a wall
+            Destroy(gameObject);
+        }
+        if(other.gameObject.tag == "GoodGuys")
+        {
             Destroy(gameObject);
         }
     }
