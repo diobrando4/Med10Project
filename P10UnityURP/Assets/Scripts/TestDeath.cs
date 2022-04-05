96abeee69375fe1d8fd83a734bb042724d4cebb9
diobrando4 <joestar373@gmail.com>
2022-04-05 21:35:07 +0200

    Added enemy death effect

--- P10UnityURP/Assets/Scripts/TestDeath.cs
+++ P10UnityURP/Assets/Scripts/TestDeath.cs
@@ -6,12 +6,6 @@ public class TestDeath : MonoBehaviour
 {
     public ParticleSystem deathParticle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
