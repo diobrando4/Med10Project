e78ad0b114c669290b71fba9d01f01bff59dc1c0
diobrando4 <joestar373@gmail.com>
2022-04-21 12:04:15 +0200

    Added open link for survey
    Added a button for the end menu that opens a url in their browser.

--- P10UnityURP/Assets/Scripts/MenuEnd.cs
+++ P10UnityURP/Assets/Scripts/MenuEnd.cs
@@ -7,6 +7,14 @@ using UnityEngine.SceneManagement; // needed to change scene
 
 public class MenuEnd : MonoBehaviour
 {
+    // url for survey goes here
+    private string url = "https://duckduckgo.com/";
+
+    public void GameOpenLink()
+    {
+        Application.OpenURL(url);
+    }
+
     public void GameReturnToMenuStart()
     {
         SceneManager.LoadScene("MenuStart");
