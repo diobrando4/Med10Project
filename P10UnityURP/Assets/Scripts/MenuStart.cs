aee41dbb55915e886a61b850c6a0db404dccf598
diobrando4 <joestar373@gmail.com>
2022-04-29 22:46:26 +0200

    Added 50/50 and lock

--- P10UnityURP/Assets/Scripts/MenuStart.cs
+++ P10UnityURP/Assets/Scripts/MenuStart.cs
@@ -11,6 +11,47 @@ public class MenuStart : MonoBehaviour
 
     // make a function that auto loads the level loader?
 
+    // just for testing
+    void Update()
+    {
+        // to active it
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            RandomGameVersion();
+        }
+        // to see the value of it
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("GetInt: " + PlayerPrefs.GetInt("GameVersion")); // default value seems to be zero
+        }
+        // to reset it
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+
+    void RandomGameVersion()
+    {
+        // this is the only way i could thick of to lock it
+        if (PlayerPrefs.GetInt("GameVersion") == 0)
+        {
+            int rnd = Random.Range(0, 2); // 0-1
+            Debug.Log("rnd: " + rnd);
+
+            if (rnd == 0)
+            {
+                PlayerPrefs.SetInt("GameVersion", 11);
+                // have game version 1 here
+            }
+            else if (rnd == 1)
+            {
+                PlayerPrefs.SetInt("GameVersion", 22);
+                // have game version 2 here
+            }
+        }
+    }
+
     public void GameStart()
     {
         levelLoader.LoadNextLevel();
