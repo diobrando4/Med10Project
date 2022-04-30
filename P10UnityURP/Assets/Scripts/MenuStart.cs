9df840bb82063addccf67580f51eab686cd51ee5
diobrando4 <joestar373@gmail.com>
2022-04-30 15:15:05 +0200

    Improved 50/50 slightly and added game version text for start menu

--- P10UnityURP/Assets/Scripts/MenuStart.cs
+++ P10UnityURP/Assets/Scripts/MenuStart.cs
@@ -2,58 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // needed to change scene
+using TMPro; // needed for text
 
 // used for buttons in the start menu
 
 public class MenuStart : MonoBehaviour
 {
     public LevelLoader levelLoader;
+    public TextMeshProUGUI gameVersionText;
 
     // make a function that auto loads the level loader?
 
+    void Start()
+    {
+        gameVersionText = GameObject.Find("Canvas/TextGameVersion").GetComponent<TextMeshProUGUI>();
+        gameVersionText.text = PlayerPrefs.GetInt("GameVersion").ToString();
+    }
+
     // just for testing
+    /*
     void Update()
     {
-        // to active it
+        // to reset it
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            //PlayerPrefs.DeleteAll();
+        }
+        // to test/active it
         if(Input.GetKeyDown(KeyCode.T))
         {
             RandomGameVersion();
         }
-        // to see the value of it
+        // to see/print the value of it
         if(Input.GetKeyDown(KeyCode.G))
         {
             Debug.Log("GetInt: " + PlayerPrefs.GetInt("GameVersion")); // default value seems to be zero
-        }
-        // to reset it
-        if(Input.GetKeyDown(KeyCode.L))
-        {
-            PlayerPrefs.DeleteAll();
+            gameVersionText.text = PlayerPrefs.GetInt("GameVersion").ToString();
         }
     }
+    */
 
     void RandomGameVersion()
     {
-        // this is the only way i could thick of to lock it
+        // this is the only way i could think of to lock it
+        // you can only change the version once because its default value is zero
         if (PlayerPrefs.GetInt("GameVersion") == 0)
         {
             int rnd = Random.Range(0, 2); // 0-1
-            Debug.Log("rnd: " + rnd);
+            //Debug.Log("rnd: " + rnd);
 
             if (rnd == 0)
             {
-                PlayerPrefs.SetInt("GameVersion", 11);
-                // have game version 1 here
+                PlayerPrefs.SetInt("GameVersion", 1);
+                //gameVersionText.text = PlayerPrefs.GetInt("GameVersion").ToString();
+
+                // have whatever variable is need to enable or disable companion dialogue
             }
             else if (rnd == 1)
             {
-                PlayerPrefs.SetInt("GameVersion", 22);
-                // have game version 2 here
+                PlayerPrefs.SetInt("GameVersion", 2);
+                //gameVersionText.text = PlayerPrefs.GetInt("GameVersion").ToString();
+
+                // have whatever variable is need to enable or disable companion dialogue
             }
         }
     }
 
     public void GameStart()
     {
+        // RandomGameVersion();
         levelLoader.LoadNextLevel();
     }
 
