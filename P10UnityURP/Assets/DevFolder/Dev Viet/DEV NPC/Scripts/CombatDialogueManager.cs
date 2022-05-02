18c8bbe7906bd254c93c20b22ef6753b72c1b970
RikimaruTsuki <tuan.viet@hotmail.com>
2022-05-02 13:36:48 +0200

    Added obstacles to Room7
    Potential bug fix on toggle dialogue

--- P10UnityURP/Assets/DevFolder/Dev Viet/DEV NPC/Scripts/CombatDialogueManager.cs
+++ P10UnityURP/Assets/DevFolder/Dev Viet/DEV NPC/Scripts/CombatDialogueManager.cs
@@ -127,6 +127,7 @@ public class CombatDialogueManager : MonoBehaviour
         else
         {
             GameObject.Find("CanvasHealthBars/TextLogBg").SetActive(false);
+            isDialogueDone = true;
         }
 
 
