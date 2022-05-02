5e6cf724420678b34d3d1815307a2a537bb8c2e2
diobrando4 <joestar373@gmail.com>
2022-05-02 13:18:14 +0200

    WIP boss

--- P10UnityURP/Assets/Scripts/EnemyBoss.cs
+++ P10UnityURP/Assets/Scripts/EnemyBoss.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+//using UnityEngine.AI;
 
 public class EnemyBoss : BaseClassEnemy
 {
@@ -15,7 +15,7 @@ public class EnemyBoss : BaseClassEnemy
         projectileSpeed = 10f;
         fireRate = 0.75f;
         //muzzle = gameObject.transform.Find("EnemyGun/Muzzle");
-        //muzzleSmoke = gameObject.transform.Find("EnemyGun/SmokeParticles").GetComponent<ParticleSystem>();
+        muzzleSmoke = gameObject.transform.Find("EnemyGun/SmokeParticles").GetComponent<ParticleSystem>();
         
         /*
         if (agent == null)
@@ -27,7 +27,12 @@ public class EnemyBoss : BaseClassEnemy
             Debug.Log("Missing NavMeshAgent on " + gameObject);
         }
         */
+        
+        StartCoroutine(BulletSpawner());
     }
+
+    public float timeBetweenBullets;
+    private float bulletCounter;
 
     // Update is called once per frame
     void Update()
@@ -35,54 +40,24 @@ public class EnemyBoss : BaseClassEnemy
         transform.Rotate(new Vector3(0, 25f, 0) * Time.deltaTime);
 
         DestroyOnDeath();
-        //Shooting();
-
-        cooldownTimer += Time.deltaTime;
-        Debug.Log(cooldownTimer);
-        if (cooldownTimer >= attackCooldown)
-        {
-            //Attack();
-        } 
     }
 
     private float attackCooldown;
     private float cooldownTimer;
     public Transform firePoint1;
+    public Transform firePoint2;
 
-    void Attack()
+    IEnumerator BulletSpawner()
     {
-        cooldownTimer = 0;
-
-        BulletController newBullet = Instantiate(bullet, firePoint1.position, firePoint1.rotation) as BulletController;
-        PlaySound("Gunshot");
-        newBullet.speed = projectileSpeed;
-        muzzleSmoke.Play();
-    }
-
-    void Shooting()
-    {
-        /*
-            shotCounter -= Time.deltaTime;
-
-            if(shotCounter <= 0)
-            {        
-                shotCounter = fireRate;
-                //BulletController newBullet = Instantiate(bullet, muzzle.position, muzzle.rotation) as BulletController;
-
-                //BulletController newBullet = Instantiate(bullet, firePoints[firePoints.Length].position, firePoints[firePoints.Length].rotation) as BulletController;
-                //BulletController newBullet = Instantiate(bullet, testFirePoint.position, testFirePoint.rotation) as BulletController;
-                //BulletController newBullet = Instantiate(bullet, firePoint1.position, firePoint1.rotation) as BulletController;
-
-                //clone = Instantiate(zombiePrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
-
-                PlaySound("Gunshot");
-                bullet.speed = projectileSpeed;
-                muzzleSmoke.Play();
-            }
-            else
-            {
-                shotCounter = 0;
-            }
-        */
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            //Debug.Log("BulletSpawner is running");
+            // needs a for loop for multiple shooting points
+            BulletController newBullet = Instantiate(bullet, firePoint1.position, firePoint1.rotation) as BulletController;
+            PlaySound("Gunshot");
+            newBullet.speed = projectileSpeed;
+            muzzleSmoke.Play();
+        }
     }
 }
