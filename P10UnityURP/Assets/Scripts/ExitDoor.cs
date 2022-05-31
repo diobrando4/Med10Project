5d3c6bc2ee41a6418da6a7dec5bd32bc357d34f0
RikimaruTsuki <tuan.viet@hotmail.com>
2022-05-31 15:54:58 +0200

    Update ExitDoor.cs

--- P10UnityURP/Assets/Scripts/ExitDoor.cs
+++ P10UnityURP/Assets/Scripts/ExitDoor.cs
@@ -22,15 +22,8 @@ public class ExitDoor : MonoBehaviour
         remainingEnemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
         //areAllEnemiesDead = false;
         combatDialogueManager = GameObject.Find("CombatDialogueManager").GetComponent<CombatDialogueManager>();
-        if (ally1 != null)
-        {
-            ally1 = GameObject.Find("AllyBlueBot").GetComponent<Ally>();
-            
-        }
-        if (ally2 != null)
-        {
-            ally2 = GameObject.Find("AllyOrangeBot").GetComponent<Ally>();
-        }
+        ally1 = GameObject.Find("AllyBlueBot").GetComponent<Ally>();
+        ally2 = GameObject.Find("AllyOrangeBot").GetComponent<Ally>();
     }
 
     // Update is called once per frame
@@ -42,12 +35,12 @@ public class ExitDoor : MonoBehaviour
 
                 // we also need to make sure the door only opens when the main dialogue is finished, maybe it should be inside its own if-statement?
                 
-                if (FindObjectOfType<SoundManager>())
+                if (ally1.isAllyDead == false && ally2.isAllyDead == false)
+                {
+                    if (FindObjectOfType<SoundManager>())
                 {
                     FindObjectOfType<SoundManager>().SoundPlay("DoorOpen");
                 }
-                if (ally1.isAllyDead == false && ally2.isAllyDead == false)
-                {
                     areAllEnemiesDead = true; // what is this used for?
                     Destroy(gameObject);
                 }
