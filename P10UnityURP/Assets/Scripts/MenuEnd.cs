a10bf50998ebf309052d79093daecc85a6824682
diobrando4 <joestar373@gmail.com>
2022-06-10 21:09:38 +0200

    WIP within-subjects

--- P10UnityURP/Assets/Scripts/MenuEnd.cs
+++ P10UnityURP/Assets/Scripts/MenuEnd.cs
@@ -8,8 +8,8 @@ using UnityEngine.SceneManagement; // needed to change scene
 public class MenuEnd : MonoBehaviour
 {
     // url for survey goes here
-    private string url1 = "https://docs.google.com/forms/d/1sg3qfpAZs9MB9u8DRa6k6nAK0yrMoPyU7uvkPZ5xlNc/edit?usp=sharing";
-    private string url2 = "https://docs.google.com/forms/d/1pUr7Y_3uczX7rx7pm9toexuj2hCZxE-6lGXXrg_tOpo/edit?usp=sharing";
+    private string url1 = "https://duckduckgo.com/1";
+    private string url2 = "https://duckduckgo.com/2";
 
     public void GameOpenLink()
     {
@@ -23,9 +23,23 @@ public class MenuEnd : MonoBehaviour
         }
     }
 
+    public void GameNextVersion() // not sure if this causes any issues if the game crashes, because then the starting version will be different?
+    {
+        if (PlayerPrefs.GetInt("GameVersion") == 1)
+        {
+            PlayerPrefs.SetInt("GameVersion", 2); // should be reversed, if we want them to play the other version afterwards?
+            SceneManager.LoadScene("Level00"); // this doesn't use transition, but it probably should?
+        }
+        if (PlayerPrefs.GetInt("GameVersion") == 2)
+        {
+            PlayerPrefs.SetInt("GameVersion", 1);
+            SceneManager.LoadScene("Level00"); // this doesn't use transition, but it probably should?
+        }
+    }
+
     public void GameReturnToMenuStart()
     {
-        SceneManager.LoadScene("MenuStart");
+        SceneManager.LoadScene("MenuStart"); // this doesn't use transition, but it probably should?
     }
 
     public void GameQuit()
